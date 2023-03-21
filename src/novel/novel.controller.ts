2c5da0adf0390cfa7ea9a8a5d04cb5acf541bf39
hye-on <ain0103@naver.com>
2023-03-21 20:59:51 +0900

    feat : sentence 프론트

--- src/novel/novel.controller.ts
+++ src/novel/novel.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseGuards,
   Req,
+  Render,
 } from '@nestjs/common';
 import { NovelService } from './novel.service';
 import { CreateNovelDto } from './dto/create-novel.dto';
@@ -23,11 +24,6 @@ export class NovelController {
     return await this.novelService.create(createNovelDto);
   }
 
-  @Get()
-  async findAll() {
-    return await this.novelService.findAll();
-  }
-
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.novelService.findOne(id);
@@ -36,61 +32,5 @@ export class NovelController {
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.novelService.remove(id);
-  }
-}
-import {
-  Controller,
-  Render,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { NovelService } from './novel.service';
-import { CreateNovelDto } from './dto/create-novel.dto';
-import { UpdateNovelDto } from './dto/update-novel.dto';
-
-@Controller('novel')
-export class NovelController {
-  constructor(private readonly novelService: NovelService) {}
-
-  @Post()
-  create(@Body() createNovelDto: CreateNovelDto) {
-    return this.novelService.create(createNovelDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.novelService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.novelService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNovelDto: UpdateNovelDto) {
-    return this.novelService.update(+id, updateNovelDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.novelService.remove(+id);
-  }
-
-  @Get('/relay/write')
-  @Render('write.ejs')
-  async view() {
-    let novel: { title: string; count: number }[] = [
-      { title: '어린왕자', count: 4 },
-      { title: '슬램덩크', count: 3 },
-      { title: '인어공주', count: 5 },
-      { title: '아기돼지3형제', count: 1 },
-    ];
-
-    return { novelList: novel };
   }
 }
--- src/sentence/sentence.controller.ts
+++ src/sentence/sentence.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Render,
+} from '@nestjs/common';
 import { SentenceService } from './sentence.service';
 import { CreateSentenceDto } from './create-sentence.dto';
 
@@ -16,13 +24,24 @@ export class SentenceController {
     return await this.sentenceService.findAll();
   }
 
-  @Get('/:novelId')
+  @Get('write/:novelId')
+  @Render('write.ejs')
   async findByNovel(@Param('novelId') novelId: string) {
-    return await this.sentenceService.findByNovel(novelId);
+    let novel: any[] = await this.sentenceService.findByNovel(novelId);
+    return { novelList: novel };
   }
 
   @Delete('/:novelId')
   async deleteSentence(@Param('novelId') novelId: string) {
     return await this.sentenceService.deleteSentence(novelId);
   }
+
+  // @Get('relay/write')
+  // @Render('write.ejs')
+  // async findAll() {
+  //   let novel: { title: string; count: number }[] =
+  //     await this.novelService.findAll();
+
+  //   return { noveContent: novel };
+  // }
 }
